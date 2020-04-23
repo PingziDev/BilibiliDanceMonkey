a05d7a8685e7698f1a5f2099a664d4b980cee648
vita2333 <lp_vitadolce@163.com>
2020-04-23 17:32:57 +0800

    eslint

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -6,10 +6,8 @@
 
 <script>
 export default {
-  data () {
-    return {}
-  }
-}
+  data() {
+    return {};
+  },
+};
 </script>
-
-
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,15 +1,16 @@
-import Vue from 'vue'
-import App from './App'
-import store from '../store'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import store from '../store';
+import router from './router';
 
-global.browser = require('webextension-polyfill')
-Vue.prototype.$browser = global.browser
+global.browser = require('webextension-polyfill');
+
+Vue.prototype.$browser = global.browser;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   store,
   router,
-  render: h => h(App)
-})
+  render: h => h(App),
+});
