dc41cce7be45a61f8a2232334f6ce556c10f06ba
vita2333 <lp_vitadolce@163.com>
2020-04-24 09:31:34 +0800

    add shims

---
+++ src/content-script/App.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<h1>Optionshahahahahha</h1>
+	</div>
+</template>
+
+<script>
+  
+  export default {
+    name: 'App',
+    mounted() {
+      console.log('45457457578===', 45457457578);
+    },
+  };
+</script>
+
+<style scoped>
+	p {
+		font-size: 20px;
+	}
+</style>
